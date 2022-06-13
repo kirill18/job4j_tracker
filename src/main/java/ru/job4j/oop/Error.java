1664756444a8ecd1b33c69b3ff00a7d870ec2543
kirill18 <kirill_lityuchii@mail.ru>
2022-06-13 13:20:34 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.print("Активно: " + active + " ");
+        System.out.print("Статус: " + status + " ");
+        System.out.print("Опсиание: " + message + " ");
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        Error fatal = new Error(true, 1, "Fatal error");
+        Error notFatal = new Error();
+        fatal.printInfo();
+        notFatal.printInfo();
+
+    }
+}
