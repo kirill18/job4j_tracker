2426de61259c5980138064ef9271483820bdc7c6
kirill18 <kirill_lityuchii@mail.ru>
2022-09-15 10:53:23 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -12,6 +12,15 @@ public class Item {
     public Item() {
     }
 
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Item{"
@@ -21,38 +30,23 @@ public class Item {
                 + '}';
     }
 
-    public Item(String name) {
-
-        this.name = name;
-    }
-
-    public Item(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
     public int getId() {
-
         return id;
     }
 
     public void setId(int id) {
-
         this.id = id;
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 
     public LocalDateTime getDateTime() {
-
         return created;
     }
 }
